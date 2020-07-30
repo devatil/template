7bc96cfa71a6e9c3b8e28da38d525ec589a9aba8
devatil <ngregister@outlook.com>
2020-07-31 01:31:59 +0300

    add Dom manupaltion

--- --template/script.js
+++ --template/script.js
@@ -1,1 +1,23 @@
-console.log('testing');
+const quoteContainer = document.getElementById('.quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+
+// Get Quote form Api
+
+async function getQuote() {
+	const proxyUrl = 'https://rocky-retreat-13655.herokuapp.com/';
+	const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+	try {
+		const response = await fetch(proxyUrl + apiUrl);
+		const data = await response.json();
+
+		quoteText.innerText = data.quoteText;
+		quoteAuthor.innerText = data.quoteAuthor;
+	} catch (error) {
+		getQuote();
+		console.log('upps doenst working this quote generator', error);
+	}
+}
+
+getQuote();
